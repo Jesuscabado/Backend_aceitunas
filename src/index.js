8a1e27223eea553fd3e896278ff20f28a28e985d
Jesuscabado <jesuscabado@hotmail.com>
2023-10-26 17:19:25 +0200

    añadido css basico a la pagina

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from 'express';//importamos express 
 import dotenv from 'dotenv';//importamos dotenv para poder usar el metodo config
 
-import router from './routes/router.js';
+import router from './routes/router.js';//importamos el archivo router.js que esta en la carpeta routes
 
 dotenv.config();//configuramos dotenv para que pueda leer el archivo .env
 const app = express();//creamos una constante app que sera igual a express
@@ -12,16 +12,16 @@ app.set('views', './src/views');//usamos el metodo set de express para que use l
 app.set('view engine', 'pug');//usamos el metodo set de express para que use el motor de plantillas pug
 
 app.use(express.json());//usamos el metodo use de express para que use el metodo json
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true})); // usamos el metodo use de express para que use el metodo urlencoded
 
-app.get('/', (req, res) => {//
-    res.render('home')
+app.get('/', (req, res) => {//usamos el metodo get de express para que cuando se introduzca / se ejecute la funcion que renderiza el archivo home.pug
+    res.render('home')// renderizamos el archivo home.pug
     });
 
 
 app.use('/', router);// usamos el metodo use de express para que cuando se introduzca / se ejecute el archivo router.js
 
 
-app.listen(3000, () => {
+app.listen(3000, () => {//usamos el metodo listen de express para que el servidor escuche en el puerto 3000
   console.log('servidor iniciado en el puerto 3000');
 });
